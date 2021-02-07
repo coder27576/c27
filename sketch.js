cf7675dac54bb8eaf9cdc7e5f670d8fba11386e4
coder27576 <harshal.bhandari1007@gmail.com>
2021-02-07 13:52:26 +0530

    added instructions

--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var chain;
+var log6;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,6 +37,9 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    log6=new Log(200,190,80,PI/2);
+    chain=new Chain(bird.body,log6.body);
+    
 
 }
 
@@ -61,4 +66,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    log6.display();
+    chain.display();
 }
